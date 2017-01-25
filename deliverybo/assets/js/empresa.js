3bc760278cdc4b2a8bf7777075552805fc96963b
facundokpo04 <facundokpo04@gmail.com>
2017-01-25 20:22:46 -0300

    bo empresa view 1.3

--- deliverybo/assets/js/empresa.js
+++ deliverybo/assets/js/empresa.js
@@ -20,12 +20,73 @@ iniciar = function (idEmpresa) {
             $('#txtEmail').val(res.Email);//select
             //ajax para traer todos los estados
             $('#txtPais').val(res.Pais);
-             $('#imagen').attr('src', '../assets/imagenes/empresa/'+res.logo);;
-            
-
+            $('#imagen').attr('src', '../assets/imagenes/empresa/' + res.logo);
+            ;
 
         }
     });
+
+
+    $('#tbSucursales').DataTable({
+        "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "Todo"]],
+        'paging': true,
+        'info': true,
+        'filter': true,
+        'stateSave': true,
+        'ajax': {
+            "url": baseurl + "index.php/sucursal/get_sucursales/1",
+            "type": "POST",
+            "dataType": 'json',
+            "data": {'<?php echo $this->security->get_csrf_token_name(); ?>': '<?php echo $this->security->get_csrf_hash(); ?>'},
+
+        },
+        'columns': [
+
+            {data: 'suc_id', 'sClass': 'dt-body-center'},
+            {data: 'suc_nombre'},
+            {data: 'suc_cuit'},
+            {data: 'suc_razonSocial'},
+            {data: 'suc_direccion'},
+            {"orderable": true,
+                render: function (data, type, row) {
+                    debugger;
+                    return '<span class="pull-right" >' +
+                            '<div class="dropdown">' +
+                            '  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
+                            '    Acciones' +
+                            '  <span class="caret"></span>' +
+                            '  </button>' +
+                            '    <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">' +
+                            '    <li><a href="#" title="Editar informacion"   onClick="cargarDataSucursal(\'' + row.suc_id + '\');"><i style="color:#555;" class="glyphicon glyphicon-edit"></i> Editar</a></li>' +
+                            '    <li><a href="#"><i class="glyphicon glyphicon-eye-open" style="color:#006699"></i> Ver</a></li>' +
+                            '    <li><a href="#" title="Eliminar Sucursal" onClick=""><i style="color:red;" class="glyphicon glyphicon-remove"></i> Eliminar</a></li>' +
+                            '    </ul>' +
+                            '</div>' +
+                            '</span>';
+                    // '<a href="#" class="btn btn-block btn-primary btn-sm" style="width: 80%;" data-toggle="modal" data-target="#modalEditCategoria" onClick="selCategoria(\'' + row.cat_id + '\');"><i class="fa fa-fw fa-edit"></i></a></td>';
+
+
+                }
+
+            }
+
+        ],
+        "columnDefs": [
+
+            {
+                "targets": [1],
+                "data": "suc_nombre",
+                "orderData": [1, 0],
+                "render": function (data, type, row) {
+                    return "<span style='color:#006699;'><i class='fa fa-home'></i>&nbsp;&nbsp;" + data + "</span>"
+
+                }
+            },
+        ],
+        "order": [[0, "asc"]],
+    });
+
+
 
 };
 
@@ -60,7 +121,6 @@ $('#btnUpdEmpresa').click(function () {
             processData: false,
             contentType: false,
             success: function (res) {
-             
 
                 location.reload();
             }
@@ -79,12 +139,12 @@ $('#btnUpdEmpresa').click(function () {
                 Email: $('#txtEmail').val(),
                 Pais: $('#txtPais').val(),
                 emp_id: $('#idEmpresa').val()
-               
+
             },
             success: function (res) {
 
 
-               
+
 
                 location.reload();
             }
